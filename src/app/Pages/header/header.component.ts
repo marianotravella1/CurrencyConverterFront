edea106a51b9d9eaa103fe4d9bd0c512f857836e
marianotravella <marianotravella2@gmail.com>
2024-11-07 06:20:03 -0300

    responsive header (mobile) 1.2

--- src/app/Pages/header/header.component.ts
+++ src/app/Pages/header/header.component.ts
@@ -6,8 +6,27 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterModule, RouterOutlet],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  toggleNav() {
+    const toggleButton = document.getElementById('nav-toggle');
+    const menu = document.getElementById('menu');
 
+    if (toggleButton && menu) {
+      menu.style.left = '100%'; 
+
+      const toggleMenu = (): void => {
+        if (menu.style.left === '0px') {
+          menu.style.left = '100%'; 
+        } else {
+          menu.style.left = '0'; 
+        }
+      };
+
+      toggleButton.addEventListener('click', toggleMenu);
+
+     
+    }
+  }
 }
