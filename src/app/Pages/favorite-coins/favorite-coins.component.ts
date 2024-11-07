860edf266ee872d12917589e1c980a43f8c9cd8c
marianotravella <marianotravella2@gmail.com>
2024-11-07 04:24:21 -0300

    header dragging and selection disabled

--- src/app/Pages/favorite-coins/favorite-coins.component.ts
+++ src/app/Pages/favorite-coins/favorite-coins.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-favorite-coins',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './favorite-coins.component.html',
   styleUrl: './favorite-coins.component.scss'
 })
