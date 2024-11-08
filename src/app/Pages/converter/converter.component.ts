66db00b460bbe694f4eb9c20bb6458e5ef86637f
marianotravella <marianotravella2@gmail.com>
2024-11-08 03:45:04 -0300

    Converter component initial view

--- src/app/Pages/converter/converter.component.ts
+++ src/app/Pages/converter/converter.component.ts
@@ -9,5 +9,19 @@ import { RouterModule } from '@angular/router';
   styleUrl: './converter.component.scss'
 })
 export class ConverterComponent {
-
+  validateInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    let value = input.value;
+  
+    // Reemplazar comas por puntos (si se prefiere solo un separador decimal)
+    value = value.replace(',', '.');
+  
+    // Permitir solo números y un solo punto decimal
+    if (!/^\d*\.?\d*$/.test(value)) {
+      value = value.slice(0, -1); // Elimina el último caracter si no es válido
+    }
+  
+    // Asigna el valor validado al input
+    input.value = value;
+  }
 }
