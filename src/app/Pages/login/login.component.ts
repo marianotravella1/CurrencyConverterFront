ff389a0b7abf3426b7055ee95d53d358263fb1b8
marianotravella <marianotravella2@gmail.com>
2024-11-09 02:21:57 -0300

    first steps: login component

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthenticationService } from '../../Services/authentication.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  authService = inject(AuthenticationService);
+  router = inject(Router);
 
+  errorLogin = false;
+
+  async login(loginForm: NgForm){
+    const {username, password} = loginForm.value;
+    const loginData = {username, password};
+    
+    const res = await this.authService.login(loginData)
+
+    if(res?.statusText === "OK") this.router.navigate(['/converter']);
+    
+    else this.errorLogin = true;
+  }
 }
