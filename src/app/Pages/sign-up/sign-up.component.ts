42af0f89498cf5721aab5f8e84fde70d4680bb5f
marianotravella <marianotravella2@gmail.com>
2024-11-09 02:22:35 -0300

    first steps: sign-up component

--- src/app/Pages/sign-up/sign-up.component.ts
+++ src/app/Pages/sign-up/sign-up.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthenticationService } from '../../Services/authentication.service';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  errorRegister = false;
+  authService = inject(AuthenticationService)
+  router = inject(Router)
 
+  async signUp(registerForm: NgForm){
+    const {username, name, email, password, subscription} = registerForm.value;
+    const registerData = {username, name, email, password, subscription};
+    
+    const res = await this.authService.signUp(registerData)
+
+    if(res?.statusText === "Created") {
+      this.router.navigate(['/login']).then(() => {
+        //Swal.fire("Registro exitoso", "", "success");
+        console.log("Registro exitoso");
+      })
+    } else this.errorRegister = true;
+  }
 }
