25933883a3da6d11f0e19ac7608919c2474b5da1
marianotravella <marianotravella2@gmail.com>
2024-11-11 22:40:48 -0300

    modified: currency service (hardcoded data)

--- src/app/Services/currency.service.ts
+++ src/app/Services/currency.service.ts
@@ -2,40 +2,39 @@ import { Injectable } from '@angular/core';
 import { Currency } from '../Interfaces/currency';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CurrencyService {
-  currencies: Currency[] = 
-  [{
-    legend: "US Dollar",
-    code: 'USD',
-    symbol: '$',
-    convertibilityIndex: '1',
-    isFavorite: true
-  },
-  {
-    legend: "US Dollar",
-    code: 'USD',
-    symbol: '$',
-    convertibilityIndex: '1',
-    isFavorite: true
-  },
-  {
-    legend: "US Dollar",
-    code: 'USD',
-    symbol: '$',
-    convertibilityIndex: '1',
-    isFavorite: true
-  },
-  {
-    legend: "US Dollar",
-    code: 'USD',
-    symbol: '$',
-    convertibilityIndex: '1',
-    isFavorite: true
-  },
-  
-]
+  currencies: Currency[] = [
+    {
+      legend: 'US Dollar',
+      code: 'USD',
+      symbol: '$',
+      convertibilityIndex: '1',
+      isFavorite: true,
+    },
+    {
+      legend: 'Argentine Peso',
+      code: 'ARS',
+      symbol: '$',
+      convertibilityIndex: '0.002',
+      isFavorite: true,
+    },
+    {
+      legend: 'Euro',
+      code: 'EUR',
+      symbol: '€',
+      convertibilityIndex: '1.09',
+      isFavorite: true,
+    },
+    {
+      legend: 'British Pound',
+      code: 'GBP',
+      symbol: '£',
+      convertibilityIndex: '1.1',
+      isFavorite: true,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 }
