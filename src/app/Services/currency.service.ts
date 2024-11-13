d82a3728571d88215af58c6da01fc154e270f237
marianotravella <marianotravella2@gmail.com>
2024-11-13 17:36:49 -0300

    get currencies fetch

--- src/app/Services/currency.service.ts
+++ src/app/Services/currency.service.ts
@@ -1,40 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Currency } from '../Interfaces/currency';
+import { environment } from '../Environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CurrencyService {
-  currencies: Currency[] = [
-    {
-      legend: 'US Dollar',
-      code: 'USD',
-      symbol: '$',
-      convertibilityIndex: '1',
-      isFavorite: true,
-    },
-    {
-      legend: 'Argentine Peso',
-      code: 'ARS',
-      symbol: '$',
-      convertibilityIndex: '0.002',
-      isFavorite: true,
-    },
-    {
-      legend: 'Euro',
-      code: 'EUR',
-      symbol: '€',
-      convertibilityIndex: '1.09',
-      isFavorite: true,
-    },
-    {
-      legend: 'British Pound',
-      code: 'GBP',
-      symbol: '£',
-      convertibilityIndex: '1.1',
-      isFavorite: true,
-    },
-  ];
+  
+  constructor() {
+    this.loadData()
+   }
 
-  constructor() {}
+  async loadData() {
+    await this.getCurrencies()
+  }
+
+  currencies: Currency[] = []
+
+  async getCurrencies(){
+    const res = await fetch(environment.API_URL+'Currency',{
+      headers: {
+        authorization:'Bearer '+localStorage.getItem("authToken")
+      },
+    })
+    if(res.status !== 200) return;
+    const resJson:Currency[] = await res.json();
+    this.currencies = resJson;
+  }
 }
