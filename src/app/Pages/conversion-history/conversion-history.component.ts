d8e338393c094de314dcbd322efeaa6e04e29907
marianotravella <marianotravella2@gmail.com>
2024-11-14 18:48:14 -0300

    modified: conversion history time formats

--- src/app/Pages/conversion-history/conversion-history.component.ts
+++ src/app/Pages/conversion-history/conversion-history.component.ts
@@ -12,12 +12,21 @@ import { ConversionService } from '../../Services/conversion.service';
 export class ConversionHistoryComponent {
   conversionService = inject(ConversionService);
 
-  formatTime(date: Date)
+  formatDate(fechaIso: any)
   {
-    const hours = date.getHours().toString().padStart(2, '0'); 
+    const fecha = new Date(fechaIso);
+    const fechaArgentina = fecha.toLocaleDateString("es-AR");
+    return fechaArgentina;
+  }
+
+  formatHour(isoDate: string): string {
+    const date = new Date(isoDate);
+    
+    // Get hours and minutes in 2-digit format (e.g., 08:05)
+    const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
-    const time = hours +':'+ minutes;
-    return time;
+  
+    return `${hours}:${minutes}`;
   }
 
 }
