13fbed8a24087ec0d5a61220c8eed3a625d98202
marianotravella <marianotravella2@gmail.com>
2024-11-17 22:42:22 -0300

    subscription service structured

--- src/app/Services/subscription.service.ts
+++ src/app/Services/subscription.service.ts
@@ -4,32 +4,28 @@ import { SignUp } from '../Interfaces/sign-up';
 import { environment } from '../Environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SubscriptionService {
-
   constructor() {
-    this.loadData()
-   }
-
-  async loadData() {
-    await this.getSubscriptions()
+    this.loadData();
   }
 
+  async loadData() {
+    await this.getSubscriptions();
+  }
 
-  subscriptions: Subscription[] = []
+  subscriptions: Subscription[] = [];
 
-  async getSubscriptions()
-  {
-    const res = await fetch(environment.API_URL+'Subscription',{
+  async getSubscriptions() {
+    const res = await fetch(environment.API_URL + 'Subscription', {
       headers: {
-        authorization:'Bearer '+localStorage.getItem("authToken")
+        authorization: 'Bearer ' + localStorage.getItem('authToken'),
       },
-    })
-    if(res.status !== 200) return;
-    const resJson:Subscription[] = await res.json();
+    });
+    if (res.status !== 200) return;
+    const resJson: Subscription[] = await res.json();
     this.subscriptions = resJson;
-    console.log(resJson)
   }
 
   
